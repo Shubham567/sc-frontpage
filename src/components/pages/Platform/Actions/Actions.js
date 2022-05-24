f1e1134b1cddc90c53715bc69749ae8cacb1e188
Amrit Anand <amrit.anand@s759labs.com>
2022-05-24 16:24:24 +0530

    wrapping some component with EffectAppearOnView

--- src/components/pages/Platform/Actions/Actions.js
+++ src/components/pages/Platform/Actions/Actions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ActionCard from "./ActionCard";
+import EffectAppearOnView from "../../../Effects/EffectAppearOnView";
 
 const actionHandler = () => {
   console.log("Button Clicked")
@@ -8,11 +9,13 @@ const actionHandler = () => {
 const Actions = () => {
   return (
     <div style={{borderLeft: "36px solid #F28705", borderRight: "36px solid #F28705"}} className="pt-24 pb-24 border-l-8 border-l-yellow">
-    <div className="pt-16 flex justify-evenly items-center flex-col xl:flex-row xl:pl-16 xl:pr-16 pb-16 bg-orange-light">
-      {
-        rawData.map((item) => (<ActionCard key={item.id} {...item} />))
-      }
-    </div>
+   <EffectAppearOnView>
+     <div className="pt-16 flex justify-evenly items-center flex-col xl:flex-row xl:pl-16 xl:pr-16 pb-16 bg-orange-light">
+       {
+         rawData.map((item) => (<ActionCard key={item.id} {...item} />))
+       }
+     </div>
+   </EffectAppearOnView>
     </div>
   );
 };
