9fceb6acde937453dcb9234a1e93d717872530da
Amrit Anand <amrit.anand@s759labs.com>
2022-05-24 16:24:24 +0530

    wrapping some component with EffectAppearOnView

--- src/components/pages/Platform/Features/FeaturesCard.js
+++ src/components/pages/Platform/Features/FeaturesCard.js
@@ -2,22 +2,25 @@ import React from 'react';
 import Image from "next/image"
 import clsx from "clsx";
 import PropTypes from "prop-types";
+import EffectAppearOnView from "../../../Effects/EffectAppearOnView";
 
 const FeaturesCard = ({direction, heading, image, description}) => {
   return (
-    <div className={clsx("flex items-center justify-between mt-12 xl:mt-20", direction)}>
-      <div className="w-full flex items-center justify-center p-4 xl:w-1/2">
-        {
-          image && image
-        }
+    <EffectAppearOnView>
+      <div className={clsx("flex items-center justify-between mt-12 xl:mt-20", direction)}>
+        <div className="w-full flex items-center justify-center p-4 xl:w-1/2">
+          {
+            image && image
+          }
+        </div>
+        <div className="w-full p-4 xl:w-1/2 flex justify-center items-center">
+          <div className="xl:p-12 xl:pl-32 xl:pr-32">
+            <h2 className="font-bold text-justify text-2xl text-dark">{heading && heading}</h2>
+            <p className="text-justify text-dark text-md mt-4">{description && description}</p>
+          </div>
+        </div>
       </div>
-      <div className="w-full p-4 xl:w-1/2 flex justify-center items-center">
-       <div className="xl:p-12 xl:pl-32 xl:pr-32">
-         <h2 className="font-bold text-justify text-2xl text-dark">{heading && heading}</h2>
-         <p className="text-justify text-dark text-md mt-4">{description && description}</p>
-       </div>
-      </div>
-    </div>
+    </EffectAppearOnView>
   );
 };
 
