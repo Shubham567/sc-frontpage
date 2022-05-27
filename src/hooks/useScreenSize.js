c7401697366b3c11a8b24138935e996eed1095cb
shubham567 <shubhamshekhar567@gmail.com>
2022-05-27 16:59:51 +0530

    fix of obj change

--- src/hooks/useScreenSize.js
+++ src/hooks/useScreenSize.js
@@ -2,15 +2,33 @@ import {useEffect, useState} from "react";
 
 const extractorOfUsefulData = ({availWidth,availHeight,height,width}) => ({availWidth,availHeight,height,width})
 
+class ScreenObj{
+  constructor(obj) {
+    try{
+      this.height = obj.height;
+      this.width = obj.width;
+      this.availHeight = obj.availHeight;
+      this.availWidth= obj.availWidth;
+    }
+    catch (e) {
+      this.height = 400;
+      this.width = 500;
+      this.availHeight = 400;
+      this.availWidth= 500;
+    }
+  }
+}
+
 const useScreenSize = () => {
 
-  const [screen,setScreen] = useState( {height : 400, width: 500, availHeight: 400, availWidth: 500});
+  const [screen,setScreen] = useState( new ScreenObj());
 
   useEffect(() => {
     setScreen(extractorOfUsefulData(window.screen));
 
     window.addEventListener("resize", e => {
-      setScreen(window.screen);
+      console.log("screen resize event");
+      setScreen(new ScreenObj(window.screen));
     })
 
   }, [])
