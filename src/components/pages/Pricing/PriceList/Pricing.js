c89a0252391b51b60165e514576d50108f77b073
Amrit Anand <amrit.anand@s759labs.com>
2022-06-02 16:15:55 +0530

    added top section and tooltip component and pricing page

--- src/components/pages/Pricing/PriceList/Pricing.js
+++ src/components/pages/Pricing/PriceList/Pricing.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import PricingCard from "./PricingCard";
-import {useSelector} from "react-redux";
 
-const Pricing = ({pricingCardData}) => {
-  const isMonthlySubscription = useSelector((state) => state.toggleReducer.monthlySubscription);
-  console.log(isMonthlySubscription);
+const Pricing = () => {
   return (
-    <div className="flex mb-32 -mt-52 items-center justify-center w-full z-10">
-      <div className="w-full flex justify-center items-stretch flex-wrap">
-        {
-          pricingCardData?.map((item) => (
-            <PricingCard key={item.id} {...item} />
-          ))
-        }
-        {/*<PricingCard/>*/}
-        {/*<PricingCard />*/}
-        {/*<PricingCard pro />*/}
-        {/*<PricingCard />*/}
+    <div className="flex -mt-52 items-center justify-center w-full z-10">
+      <div className="w-full xl:w-11/12 flex justify-center items-center flex-wrap">
+        <PricingCard/>
+        <PricingCard />
+        <PricingCard />
       </div>
     </div>
   );
--- src/components/pages/Pricing/PriceList/PricingCard.js
+++ src/components/pages/Pricing/PriceList/PricingCard.js
@@ -3,69 +3,38 @@ import Image from "next/image";
 import Button from "../../../Button";
 import EffectAppearOnView from "../../../Effects/EffectAppearOnView";
 import ToolTip from "../../../ToolTip";
-import PricingTopBg from "../../../svgs/PricingTopBg";
-import clsx from "clsx";
-import {useSelector} from "react-redux";
 
-const PricingCard = ({id, type, topSubHeading, priceMonth, priceYear, heading, subHeading, features}) => {
-  const isMonthlySubscription = useSelector((state) => state.toggleReducer.monthlySubscription);
-  console.log(isMonthlySubscription);
+const PricingCard = () => {
   return (
-    <div className={"w-full relative sm:w-1/2 md:w-1/3 xl:w-1/5 bg-white m-6 rounded-xl overflow-hidden"}
-         style={{boxShadow: "4px 4px 16px rgba(0, 0, 0, 0.2)"}}>
-      <div className="relative">
-        <div className="w-full">
-          {/*<Image src="/images/pricing/pricingCardCurve.svg" layout="responsive" height="301px" width="466px"/>*/}
-          <PricingTopBg width="100%"
-                        className={clsx("absolute -top-4", (type === "Pro") ? "fill-orange" : "fill-primary")}/>
-        </div>
-        <div className="absolute w-full h-24">
-          <div className="flex h-full flex-wrap w-full p-4 items-center justify-between">
-            <div className="text-white">
-              <h3 className="text-2xl text-white font-medium">{type && type}</h3>
-              <p className="text-xs font-medium">{topSubHeading && topSubHeading}</p>
-            </div>
-            <h2 className="text-4xl text-white mt-1 font-bold">
-              {
-                isMonthlySubscription ? (
-                  priceMonth === "Free" ? <p>Free<sup className="text-base font-light">/forever</sup></p> : priceMonth ?
-                    <p><span className="text-xl">$</span>{priceMonth}<sup className="text-base font-normal">/Month</sup>
-                    </p> : <a className="text-base flex cursor-pointer font-light">Contact Us</a>
-                ) : (
-                  priceYear === "Free" ? <p>Free<sup className="text-base font-light">/forever</sup></p> : priceYear ?
-                    <p><span className="text-xl">$</span>{priceYear}<sup className="text-base font-normal">/Year</sup>
-                    </p> : <a className="text-base flex cursor-pointer font-light">Contact Us</a>
-                )
-              }
-            </h2>
-          </div>
-        </div>
-      </div>
-      <div className="p-4 h-full flex flex-col justify-between pt-48 pt-0">
-        <div className="mb-6">
-          <EffectAppearOnView><h3 className="text-2xl font-bold">{heading && heading}</h3></EffectAppearOnView>
-          <EffectAppearOnView><p className="text-xl font-medium mb-4">{subHeading && subHeading}</p></EffectAppearOnView>
-          {
-            features.map((item) => (
-              <EffectAppearOnView key={item.id}><div className="mt-2 flex items-center text-lg"><ToolTip
-                tooltipHeading={item.tooltipHeading} iconsStyle={"text-primary"} disabled={item?.disabled}
-                tooltipDescription={item.tooltipDescription}/> &nbsp; <span className={clsx("ml-2",item?.disabled ? "text-gray" : "text-dark")}>{item?.title}</span></div></EffectAppearOnView>
-            ))
-          }
+     <div className={"w-full sm:w-1/2 md:w-1/3 xl:w-1/4 bg-white m-8 rounded-xl overflow-hidden"} style={{boxShadow: "4px 4px 16px rgba(0, 0, 0, 0.2)"}}>
+       <div className="relative">
+         <div className="w-full">
+           <Image src="/images/pricing/pricingCardCurve.svg" layout="responsive" height="301px" width="466px"/>
+         </div>
+         {/*<EffectAppearOnView>*/}
+           <div className="absolute top-8 left-8">
+             <h3 className="text-xl text-white font-medium-bold">LOREM IPSUM</h3>
+             <h2 className="text-4xl text-white mt-1 font-bold"><span className="text-2xl">$</span>99</h2>
+           </div>
+         {/*</EffectAppearOnView>*/}
+       </div>
+        <div className="p-8 pt-0">
+          <div className="mb-6">
+            <EffectAppearOnView><h3 className="text-3xl font-bold mb-4">Lorem Ipsum</h3></EffectAppearOnView>
+            {
+              Object.keys([...Array(7)]).map((item, i) => (
+                <EffectAppearOnView key={i}><p className="mt-2 flex items-center text-xl"><ToolTip tooltipHeading={`Heading( ${i} )`} iconsStyle={"text-primary"} tooltipDescription={`Description... ( ${i} )`} /> &nbsp; Lorem Ipsum</p></EffectAppearOnView>
+              ))
+            }
 
+          </div>
+         <EffectAppearOnView>
+           <Button className="pl-8 pr-8" color="primary" variant="contained" size="lg" rounded={false}>
+             Buy Now
+           </Button>
+         </EffectAppearOnView>
         </div>
-        <div className="w-full flex justify-between flex-wrap items-center p-2">
-          {/*<EffectAppearOnViews>*/}
-          <p className="text-primary text-base cursor-pointer font-medium">
-            View Details
-          </p>
-            <Button className="pl-6 pr-6" color="primary" variant="contained" size="md" rounded={false}>
-              Buy Now
-            </Button>
-          {/*</EffectAppearOnViews>*/}
-        </div>
-      </div>
-    </div>
+     </div>
   );
 };
 
