41bc3f07d4f1f3a88f6984356ac5a44784cf201d
Amrit Anand <amrit.anand@s759labs.com>
2022-06-06 15:00:59 +0530

    added responsive view of features

--- src/components/pages/Pricing/Features/Features.js
+++ src/components/pages/Pricing/Features/Features.js
@@ -1,45 +1,61 @@
 import React from 'react';
 import SingleFeature from "./SingleFeature";
 import PropTypes from "prop-types";
+import CheckBoxIcon from "../../../svgs/CheckBoxIcon";
 
 const Feature = ({featuresData, topTableTitle}) => {
   return (
     <div id="features" className="mt-24 mb-24">
+
       <h1 className="text-center font-normal text-5xl m-4 text-primary mb-12">Features</h1>
-      <div className="w-full flex justify-center items-center">
-        <div className="w-full md:w-4/5">
-          <table className="w-full table-auto border-separate">
-            <thead>
-            <tr className="odd:bg-gray-bg">
+      <div className="hidden md:block">
+        <div className="w-full flex justify-center items-center">
+          <div className="w-full md:w-4/5">
+            <table className="w-full table-auto border-separate">
+              <thead>
+              <tr className="odd:bg-gray-bg">
+                {
+                  topTableTitle.map(item => (
+                    <th className="font-bold border-4 border-white text-left p-2 m-1"
+                        key={item.id}>{item.title && item.title}</th>
+                  ))
+                }
+              </tr>
+              </thead>
+              <tbody>
               {
-                topTableTitle.map(item => (
-                  <th className="font-bold border-4 border-white text-left p-2 m-1"
-                      key={item.id}>{item.title && item.title}</th>
+                featuresData.map(item => (
+                  <tr key={item.id} className="odd:bg-white even:bg-gray-bg">
+                    <th style={{margin: "1rem"}}
+                        className="m-4 border-4 border-white font-bold text-left p-2">{item.dataKey && item.dataKey}</th>
+                    <td style={{margin: "1rem"}}
+                        className="text-left border-4 border-white p-2">{item.free.title && item.free.title}</td>
+                    <td style={{margin: "1rem"}}
+                        className="text-left border-4 border-white p-2">{item.basic.title && item.basic.title}</td>
+                    <td style={{margin: "1rem"}}
+                        className="text-left border-4 border-white p-2">{item.pro.title && item.pro.title}</td>
+                    <td style={{margin: "1rem"}}
+                        className="text-left border-4 border-white p-2">{item.enterprise.title && item.enterprise.title}</td>
+                  </tr>
                 ))
               }
-            </tr>
-            </thead>
-            <tbody>
-            {
-              featuresData.map(item => (
-                <tr key={item.id} className="odd:bg-white even:bg-gray-bg">
-                  <th style={{margin: "1rem"}}
-                      className="m-4 border-4 border-white font-bold text-left p-2">{item.dataKey && item.dataKey}</th>
-                  <td style={{margin: "1rem"}}
-                      className="text-left border-4 border-white p-2">{item.free.title && item.free.title}</td>
-                  <td style={{margin: "1rem"}}
-                      className="text-left border-4 border-white p-2">{item.basic.title && item.basic.title}</td>
-                  <td style={{margin: "1rem"}}
-                      className="text-left border-4 border-white p-2">{item.pro.title && item.pro.title}</td>
-                  <td style={{margin: "1rem"}}
-                      className="text-left border-4 border-white p-2">{item.enterprise.title && item.enterprise.title}</td>
-                </tr>
-              ))
-            }
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+
+
+      <div className="block md:hidden">
+        <div className="flex flex-wrap justify-center">
+          {
+            topTableTitle?.map(item => (
+              <SingleFeature key={item.id} topicObj={item} details={featuresData}/>
+            ))
+          }
+        </div>
+      </div>
+
     </div>
   );
 };
@@ -47,7 +63,7 @@ const Feature = ({featuresData, topTableTitle}) => {
 export default Feature;
 
 Feature.propTypes = {
-  featuresData : PropTypes.arrayOf({
+  featuresData: PropTypes.arrayOf({
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     dataKey: PropTypes.string,
     free: PropTypes.object,
--- src/components/pages/Pricing/Features/SingleFeature.js
+++ src/components/pages/Pricing/Features/SingleFeature.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import clsx from "clsx";
 import EffectAppearOnView from "../../../Effects/EffectAppearOnView";
+import CheckBoxIcon from "../../../svgs/CheckBoxIcon";
 
-const SingleFeature = ({featureName, style}) => {
+const SingleFeature = ({details, topicObj, style}) => {
   return (
-    <div className={clsx("m-2 text-xl inline-block pl-4 sm:pl-12 pr-4 sm:pr-12 pt-1 pb-1", style)}>
-      <EffectAppearOnView>
-        {
-          featureName && featureName
-        }
-      </EffectAppearOnView>
-    </div>
+    <>
+      {
+        !(topicObj?.title === "Features") &&
+        <div style={{boxShadow: "4px 4px 16px rgba(0, 0, 0, 0.2)"}}
+             className={clsx("m-6 rounded-xl overflow-hidden mt-12 mb-12 w-full sm:w-1/2 md:w-1/3 xl:w-1/4 text-xl", style)}>
+          <div
+            className={clsx("text-center text-dark pt-2 pb-2 font-bold text-2xl pl-4 pr-4", (topicObj?.title === "Pro") ? "bg-orange-light" : "bg-primary-light")}>
+            <EffectAppearOnView>
+              {
+                topicObj?.title
+              }
+            </EffectAppearOnView>
+          </div>
+          <div className="p-4">
+            {
+              details?.map((item) => (
+                <div key={item.id}>
+                  {
+                    !(item[`${topicObj?.title?.toLowerCase()}`].title.toLowerCase() === "no") ?
+                    <EffectAppearOnView key={item.id}>
+                      <div className="flex items-center m-2">
+                    <span className="w-8 h-8 flex items-center justify-center">
+                      <CheckBoxIcon fill={topicObj?.title === "Pro" ? "orange" : "green"}/>
+                    </span>
+                        <span className="ml-2 flex text-lg">
+                      {item?.dataKey + " - " +
+                      item[`${topicObj?.title?.toLowerCase()}`].title
+                      }
+                    </span>
+                      </div>
+                    </EffectAppearOnView> : <></>
+                  }
+                </div>
+              ))
+            }
+          </div>
+          {/*<CheckBoxIcon fill={"green"} width={22} height={24} />*/}
+          {/*<CheckBoxIcon fill={"orange"} width={22} height={24} />*/}
+        </div>
+      }
+    </>
   );
 };
 
